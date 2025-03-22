e300779695384baf16bee719a2336dd8aef21222
Peter Mai <mttrung94@gmail.com>
2025-03-22 11:32:42 +0000

    Fixed the fact table and dim table to only include total at end

--- dbt_model/models/core/dim_weekly_infection.sql
+++ dbt_model/models/core/dim_weekly_infection.sql
@@ -3,8 +3,14 @@
 }}
 
 with infection_data as (
-    select * from {{ ref("fact_data_table")}} where  hbname = "Total"
+    select * from {{ ref("fact_data_table")}}
 )
 
-select *, (covidAdmissions+influenzaadmissions+rsvadmissions) as totalInfections from infection_data
+select 
+weekending,
+sum(covidAdmissions) as total_covid,
+sum(influenzaadmissions) as total_influenza,
+sum(rsvadmissions) as total_rsv,
+(sum(covidAdmissions)+sum(influenzaadmissions)+sum(rsvadmissions)) as total_infections from infection_data
+group by 1
 order by weekending
